ec15161877d0352674ce94b9169979a92fbee245
amig05c <amitghosh0595c@gmail.com>
2023-01-08 14:36:36 +0530

    todo using typescript

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+// import { Button, TextField } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+const Input: React.FC = () => {
+  return (
+    <Box border={"1px solid red"}>
+        <Box display={"flex"} justifyContent="center" gap="1rem"  border={"1px solid black"}  
+    //      sx={{
+    //     width: 500,
+    //     maxWidth: '100%',
+    //   }}
+      >
+
+         <TextField fullWidth label="fullWidth" id="fullWidth" />
+         <Button>Add</Button>
+        </Box>
+    </Box>
+  )
+}
+
+export default Input
