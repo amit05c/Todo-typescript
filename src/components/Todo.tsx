d8a8106350cc82870e40b787884b908e5db28a73
amig05c <amitghosh0595c@gmail.com>
2023-01-08 17:05:57 +0530

    todos created

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import React from 'react'
 import Input from './Input'
-
+// import { uuidv4 } from 'uuid';
+interface initial{
+    id:number,
+    title:string,
+    status:boolean,
+}
 const Todo = () => {
-    const handleClick= (newTodo:String):void=>{
-      console.log(newTodo)
+    const [todos, setTodos]= useState<Array <initial>>([])
+    const handleClick= (newTodo:string):void=>{
+     setTodos([...todos,{
+        id:Date.now(),
+        title:newTodo,
+        status:false
+     }])
     }
+    console.log(todos)
   return (
     <div>
         <Input handleClick={handleClick}/>
